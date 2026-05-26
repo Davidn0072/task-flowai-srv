2750e0b99448344ad73081485e57301fb2ca9535
Davidn0072 <Davidn0072@Gmail.COM>
2026-05-26 10:13:25 +0300

    Update project configuration paths and binary files, including changes to appsettings.json and various DLLs, while correcting directory references.

--- obj/Debug/net9.0/TaskFlowAISev.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskFlowAISev.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlowAISev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d7189eb69aca1a91ae19b44415c29d1de856d1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f100ca92fdcc4d6e03991ab1ce974e9c0d75d997")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlowAISev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlowAISev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
